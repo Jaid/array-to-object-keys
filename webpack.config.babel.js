6da0d831f0266a15b48de29260e45aa129f216af
Jaid <jaid.jsx@gmail.com>
2019-01-18 14:07:36 +0100

    Added some fields to final package.json

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,4 +2,16 @@ import webpackConfigJaid from "webpack-config-jaid"
 
 export default webpackConfigJaid({
   type: "lib",
+  publishimo: {
+    publishimoOptions: {
+      config: {
+        author: {
+          name: "Jaid",
+          email: "jaid.jsx@gmail.com",
+          github: true,
+        },
+        license: "MIT",
+      },
+    },
+  },
 })
