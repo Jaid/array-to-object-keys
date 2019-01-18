c1c460208eb3740b270713d3942df7a31f5aaa18
Jaid <jaid.jsx@gmail.com>
2019-01-18 12:31:27 +0100

    Updated webpack-config-jaid

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,15 +1,5 @@
-import path from "path"
-
 import webpackConfigJaid from "webpack-config-jaid"
-import PublishimoWebpackPlugin from "publishimo-webpack-plugin"
-
-const fromPackage = directive => path.resolve(__dirname, directive)
 
 export default webpackConfigJaid({
   type: "lib",
-  extra: {
-    plugins: [
-      new PublishimoWebpackPlugin
-    ]
-  }
 })
