0c5739d5c26744dfefd7c21dd72899a6331304b4
Jaid <jaid.jsx@gmail.com>
2019-01-21 16:04:37 +0100

    Updated webpack-config-jaid

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,18 +3,16 @@ import webpackConfigJaid from "webpack-config-jaid"
 export default webpackConfigJaid({
   type: "lib",
   include: [
-    "license.txt",
+    "license.*",
     "readme.md",
   ],
   publishimo: {
     publishimoOptions: {
-      config: {
-        author: {
-          name: "Jaid",
-          email: "jaid.jsx@gmail.com",
-          github: true,
-        },
-        license: "MIT",
+      fetchGithub: true,
+      author: {
+        name: "Jaid",
+        email: "jaid.jsx@gmail.com",
+        github: true,
       },
     },
   },
