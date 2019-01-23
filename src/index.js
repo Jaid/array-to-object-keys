d09fe0ddd16b218afd0b94c9b9c8b62ba4e13252
Jaid <jaid.jsx@gmail.com>
2019-01-23 17:59:38 +0100

    Added jsdoc annotation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/**
+ * Converts an array to an object with static keys and customizable values
+ * @example
+ * arrayToObjectKeys(["a", "b"])
+ * // {a: null, b: null}
+ * @example
+ * arrayToObjectKeys(["a", "b"], (key, index) => `value for ${key} #${index + 1}`)
+ * // {a: "value for a #1", b: "value for b #2"}
+ * @param {Array} array
+ * @param {function} function
+ * @returns {Object} A generated object based on the array input
+ */
 export default (array, valueGenerator = null) => {
   if (!Array.isArray(array)) {
     return {}
