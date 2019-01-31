2bc9e25dffdbb0cdf5d97d7a7203d243ad494f2f
Jaid <jaid.jsx@gmail.com>
2019-01-31 06:09:15 +0100

    Slightly adjusted webpack-config-jaid configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
 import webpackConfigJaid from "webpack-config-jaid"
 
 export default webpackConfigJaid({
-  documentation: {},
+  documentation: true,
   publishimo: {
     publishimoOptions: {
       fetchGithub: true,
