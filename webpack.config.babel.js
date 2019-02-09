f20f6f704e39cd34afe7da849a138d78b7ae44a8
Jaid <jaid.jsx@gmail.com>
2019-02-09 02:57:22 +0100

    Formatted
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,5 @@ import {configureLib} from "webpack-config-jaid"
 
 export default configureLib({
   documentation: true,
-  publishimo: {
-    fetchGithub: true,
-  },
+  publishimo: {fetchGithub: true},
 })
