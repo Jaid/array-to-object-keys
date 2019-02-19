e1c73b3c2b4b8ed3cdebef972d901617cb60b258
Jaid <jaid.jsx@gmail.com>
2019-02-19 23:20:07 +0100

    Implemented parallel
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,35 +9,90 @@
  */
 
 /**
+ * @typedef asyncValueGenerator
+ * @type {function}
+ * @async
+ * @param {string} value Original array entry
+ * @param {number} index Index of the array entry (starts at 0)
+ * @returns {*} Anything that will be the object entry value
+ */
+
+const emptyReturn = {}
+
+/**
  * Converts an array to an object with static keys and customizable values
  * @example
  * import arrayToObjectKeys from "array-to-object-keys"
- * arrayToObjectKeys(["a", "b"])
- * // {a: null, b: null}
+ * let result = arrayToObjectKeys(["a", "b"])
+ * result = {a: null, b: null}
  * @example
  * import arrayToObjectKeys from "array-to-object-keys"
- * arrayToObjectKeys(["a", "b"], "value")
- * // {a: "value", b: "value"}
+ * let result = arrayToObjectKeys(["a", "b"], "value")
+ * result = {a: "value", b: "value"}
  * @example
  * import arrayToObjectKeys from "array-to-object-keys"
- * arrayToObjectKeys(["a", "b"], (key, index) => `value for ${key} #${index + 1}`)
- * // {a: "value for a #1", b: "value for b #2"}
+ * let result = arrayToObjectKeys(["a", "b"], (key, index) => `value for ${key} #${index + 1}`)
+ * result = {a: "value for a #1", b: "value for b #2"}
  * @function
  * @param {string[]} array Keys for the generated object
  * @param {valueGenerator|*} [valueGenerator=null] Optional function that sets the object values based on key and index
  * @returns {object<string, *>} A generated object based on the array input
  */
 export default (array, valueGenerator = null) => {
-  if (!Array.isArray(array)) {
-    return {}
+  if (!Array.isArray(array) || !array.length) {
+    return emptyReturn
   }
   const object = {}
   if (typeof valueGenerator === "function") {
-    let index = 0
+    array.forEach((key, index) => {
+      object[key] = valueGenerator(key, index)
+    })
+  } else {
     for (const value of array) {
-      object[value] = valueGenerator(value, index)
-      index++
+      object[value] = valueGenerator
     }
+  }
+  return object
+}
+
+/**
+ * Converts an array to an object with static keys and customizable values
+ * @example
+ * import fs from "fs"
+ * import path from "path"
+ * import {parallel} from "array-to-object-keys"
+ * const keys = ["license", "readme", "package", ".travis", "not-here"]
+ * const valueGenerator = async name => {
+ *   const files = await fs.promises.readdir(path.resolve(__dirname, ".."))
+ *   for (const file of files) {
+ *     if (file.startsWith(`${name}.`)) {
+ *       const stat = await fs.promises.stat(path.resolve(__dirname, "..", file), "utf8")
+ *       return stat.size
+ *     }
+ *   }
+ *   return null
+ * }
+ * let result = await parallel(keys, valueGenerator)
+ * result = { ".travis": 1672, license: 1099, package: 1948, readme: 132, "not-here": null }
+ * @function
+ * @param {string[]} array Keys for the generated object
+ * @param {asyncValueGenerator|*} [valueGenerator=null] Async function that sets the object values based on key and index
+ * @returns {object<string, *>} A generated object based on the array input
+ */
+export const parallel = async (array, valueGenerator = null) => {
+  if (!Array.isArray(array) || !array.length) {
+    return emptyReturn
+  }
+  const object = {}
+  if (typeof valueGenerator === "function") {
+    for (const key of array) {
+      object[key] = null // Setting object keys synchronously to ensure order
+    }
+    const jobs = array.map(async (key, index) => {
+      const value = await valueGenerator(key, index)
+      object[key] = value
+    })
+    await Promise.all(jobs)
   } else {
     for (const value of array) {
       object[value] = valueGenerator
--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 import path from "path"
+import fs from "fs"
 
 const indexModule = process.env.MAIN ? path.resolve(__dirname, "..", process.env.MAIN) : path.join(__dirname, "..", "src")
-const {default: arrayToObjectKeys} = require(indexModule)
+const {default: arrayToObjectKeys, parallel} = require(indexModule)
 
 it("should run with 1 argument", () => {
   const result = arrayToObjectKeys(["a", "b"])
@@ -25,5 +26,38 @@ it("should run with a function as second argument", () => {
   expect(result).toEqual({
     a: "1-a-x",
     b: "2-b-x",
+  })
+})
+
+it("should run in parallel", async () => {
+  const valueGenerator = (key, index) => `${index + 1}-${key}-x`
+  const result = await parallel(["a", "b"], valueGenerator)
+  expect(result).toEqual({
+    a: "1-a-x",
+    b: "2-b-x",
+  })
+})
+
+it("should call fs functions in parallel", async () => {
+  const keys = ["license", "readme", "package", ".travis", "not-here"]
+  const valueGenerator = async name => {
+    const files = await fs.promises.readdir(path.resolve(__dirname, ".."))
+    for (const file of files) {
+      if (file.startsWith(`${name}.`)) {
+        const stat = await fs.promises.stat(path.resolve(__dirname, "..", file), "utf8")
+        return stat.size
+      }
+    }
+    return null
+  }
+  const result = await parallel(keys, valueGenerator)
+  expect(keys).toEqual(["license", "readme", "package", ".travis", "not-here"])
+  expect(Object.keys(result)).toEqual(keys)
+  expect(result).toMatchObject({
+    license: expect.any(Number),
+    readme: expect.any(Number),
+    package: expect.any(Number),
+    ".travis": expect.any(Number),
+    "not-here": null,
   })
 })
